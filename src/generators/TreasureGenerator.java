5a9475ef869f184ff49e39c56e34551773ecca07
Simon Blomsterlund <simon@simonblund.com>
2019-01-06 16:42:20 +0200

    Boss and treasures

--- src/generators/TreasureGenerator.java
+++ src/generators/TreasureGenerator.java
@@ -1,10 +1,20 @@
 package generators;
 
-public class TreasureGenerator {
-    public static void newPotion(){
+import game.Treasure;
+import repository.TreasureRepository;
 
+public class TreasureGenerator {
+    public static void newPotion(String name, String desc, int health){
+        Treasure np = new Treasure();
+        np.setName(name);
+        np.setDescription(desc);
+        np.setHealth(health);
+        TreasureRepository.treasures.add(np);
     }
-    public static void newWeapon(){
+    public static void newCoin(int coin){
+        Treasure nc = new Treasure();
+        nc.setCoin(coin);
+        TreasureRepository.treasures.add(nc);
 
     }
 }
--- src/repository/TreasureRepository.java
+++ src/repository/TreasureRepository.java
@@ -1,5 +1,36 @@
 package repository;
 
+import game.Treasure;
+import generators.TreasureGenerator;
+
+import java.util.ArrayList;
+
 public class TreasureRepository {
+    public static ArrayList<Treasure> treasures;
+
+
+    public static void generateTreasures(){
+        TreasureGenerator.newPotion("Aunt Olga's friday smoothie", "Just don't ask for the recipe.", 20);
+        TreasureGenerator.newPotion("Father Goran's afternoon tea", "Smells like teen spirit", 25);
+
+        TreasureGenerator.newCoin(5);
+        TreasureGenerator.newCoin(5);
+        TreasureGenerator.newCoin(5);
+        TreasureGenerator.newCoin(5);
+        TreasureGenerator.newCoin(5);
+        TreasureGenerator.newCoin(5);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+        TreasureGenerator.newCoin(10);
+
+    }
+
 
 }
