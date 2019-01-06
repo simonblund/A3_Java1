aa06d12fc6fb9db892f683959fba0788d80b5b13
Simon Blomsterlund <simon@simonblund.com>
2019-01-06 17:47:11 +0200

    Bossfight

--- src/game/Fight.java
+++ src/game/Fight.java
@@ -64,4 +64,66 @@ public class Fight {
         }
         return health - strength;
     }
+
+    public static void meetboss(Monster monster, Player player) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(utility.Artwork.devil());
+        System.out.println();
+        System.out.println("Oh dear...");
+        System.out.println("You seem to have found a way out... but...");
+        System.out.println("It is guarded by the ugliest creature you have ever seen");
+        System.out.println();
+        System.out.println("It's " + monster.getName());
+        System.out.println(monster.getDescription());
+        System.out.println("It has health: " + monster.getHealth() + " and strength: " + monster.getStrength());
+        System.out.println();
+        System.out.println("You on the other hand have, health: " + player.getHealth() + " and strength: " + player.getStrength());
+        System.out.println();
+        System.out.println("The adrenaline rush you feel however makes you a lot quicker to run away \n so if you try to do that you have a 50 percent chance of succeeding with that.");
+        System.out.println("What would you like to do?");
+        System.out.println("1. Fight the beast. \n 2. Run away screaming");
+        String choice = sc.nextLine();
+        boolean running = true;
+        while (running) {
+            switch (choice) {
+                case ("1"):
+                    fightboss(monster, player);
+                    running = false;
+                    break;
+                case ("2"):
+                    System.out.println();
+                    System.out.println("You try and run away, if the dice hits 4 or above you succeed.");
+                    int dice = utility.Dice.dice();
+                    System.out.println("You throw the dice...");
+                    System.out.println(dice);
+                    if(dice >3){
+                        System.out.println("You ran away");
+                    } else {
+                        System.out.println("Awwh... it catched you");
+                        fightboss(monster, player);
+                    }
+            }
+        }
+    }
+    public static void fightboss(Monster monster, Player player){
+        System.out.println("You get ready for the fight of the century");
+        while(player.getHealth()>0 && monster.getHealth()>0) {
+            monster.setHealth(hit(player.getStrength(), monster.getHealth()));
+            System.out.println("You have health "+player.getHealth());
+            if(monster.getHealth()<1){
+                System.out.println("You won!");
+                break;
+            }
+            System.out.println("The monster has health "+monster.getHealth());
+            player.setHealth(hit(monster.getStrength(), player.getHealth()));
+            System.out.println("You have health "+player.getHealth());
+            System.out.println("The monster has health "+monster.getHealth());
+            System.out.println("");
+        }
+        if(player.getHealth()<1){
+            GamePlay.playerDead(player);
+        } else{
+            System.out.println("Congratulations");
+        }
+    }
 }
