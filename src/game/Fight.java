1260c1593966f124ecfcff0ce777f068707d96bc
Simon Blomsterlund <simon@simonblund.com>
2019-01-06 17:50:22 +0200

    Bossfight

--- src/game/Fight.java
+++ src/game/Fight.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class Fight {
     public static void meetMonster(Monster monster, Player player, int x, int y){
-        if (monster.getHealth()>0){
+        if (monster.isBoss() && monster.getHealth()>0){
+            meetboss(monster, player);
+        }else if (monster.getHealth()>0){
             System.out.println("");
             System.out.println("");
             System.out.println("Something moves in the dark...");
