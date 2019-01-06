8640ee095ffab6d81fed3ae4790cf8c0cf644229
daniel <daniel@d.lan>
2019-01-06 14:33:29 +0100

    karta och navigation fungerar, med array som du föreslog

--- src/game/Shop.java
+++ src/game/Shop.java
@@ -3,61 +3,58 @@ package game;
 import java.util.Scanner;
 
 public class Shop {
-    public static void encounterSalesman(Player player){
+
+    public static void encounterSalesman(Player player) {
+
+
+        Scanner input = new Scanner(System.in);
+        String choice;
+
+        boolean running = true;
         
-
-            
-            
+        while (running) {
             encounterMessage();
+            choice = input.nextLine();
             
-            Scanner input = new Scanner(System.in);
-            String choice = input.nextLine();
-
-            boolean running = true;
-            while (running) {
-                switch(choice){
-                    case ("1"):
-                        browseMerchantsItems(player);
-                        encounterMessage();
-                        choice = input.nextLine();
-                        break;
-                    case ("2"):
-                        talkToMerchant();
-                        encounterMessage();
-                        choice = input.nextLine();
-                        break;
-                    case ("3"):
-                        running = false;
-                        break;
-                    default:
-                        choice = input.nextLine();
-                        break;
-                 }
+            switch (choice) {
+                case ("1"):
+                    browseMerchantsItems(player);
+                    break;
+                case ("2"):
+                    talkToMerchant();
+                    break;
+                case ("3"):
+                    running = false;
+                    break;
+                default:
+                    break;
             }
+        }
+        
+    } // end method encounterSalesman()
 
-    }
-
-    public static void browseMerchantsItems(Player player){
+    public static void browseMerchantsItems(Player player) {
         boolean browse = true;
 
-        while (browse){
+        while (browse) {
 
             System.out.println("1. Buy a health potion for 50 coin.");
-            System.out.println("2. Leave.");
+            System.out.println("2. Stop shopping.");
 
             Scanner input = new Scanner(System.in);
             int choice = input.nextInt();
 
-            switch (choice){
+            switch (choice) {
 
                 case (1):
 
-                    if (player.getGold() >= 50){
-                        player.setGold(player.getGold()-50);
-                        player.setHealthPotions(player.getHealthPotions() +1);
+                    if (player.getGold() >= 50) {
+                        player.setGold(player.getGold() - 50);
+                        player.setHealthPotions(player.getHealthPotions() + 1);
                         System.out.println("You bought a healt potion. You now have" + player.getHealthPotions() + " healt potions in your inventory.");
+                    } else {
+                        System.out.println("You dont have enough coin.");
                     }
-                    else {System.out.println("You dont have enough coin.");}
                     break;
 
                 case (2):
@@ -72,14 +69,14 @@ public class Shop {
         }
 
     }//EOXbrowseMerchantsitems
-    
-    public static void talkToMerchant(){
-     System.out.println("Hey, what are you doing here?");
+
+    public static void talkToMerchant() {
+        System.out.println("Hey, what are you doing here?");
     }
-    
-    public static void encounterMessage(){
-        System.out.println("You come across a salesman... which is a bit strange considering the surroundings. \n"
+
+    public static void encounterMessage() {
+        System.out.println("You have come across a salesman... which is a bit strange... considering the surroundings. \n"
                 + "Type 1 to look at the items for sale. Type 2 to talk to the salesman. Type 3 to leave.");
-        
+
     }
 }
